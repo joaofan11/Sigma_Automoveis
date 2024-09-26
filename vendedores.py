65810ac18308cd6cae39912eeba8c668a9f61e5b
gabriel <gabrielfranco848@gmail.com>
2024-09-26 15:05:22 -0300

    Delete vendedores.py

--- vendedores.py
+++
@@ -1,7 +0,0 @@
-class Vendedores:
-    arquivo = "database/vendedores.txt"
-
-    @staticmethod
-    def menu():
-        print("Menu de Vendedores")
-        # Implementar opções de cadastro, busca, etc.
