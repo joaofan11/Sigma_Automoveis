411d151c3aaa6d70185eac653767c2039be24d27
gabriel <gabrielfranco848@gmail.com>
2024-09-26 15:07:00 -0300

    Delete veiculos.py

--- veiculos.py
+++
@@ -1,7 +0,0 @@
-class Veiculos:
-    arquivo = "database/veiculos.txt"
-
-    @staticmethod
-    def menu():
-        print("Menu de Veículos")
-        # Implementar funcionalidades como cadastrar, listar, buscar veículos
